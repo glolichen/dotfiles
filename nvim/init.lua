af1b39f78de8ff52bf0a05c31a7c669fdb93d7d1
Jayden Li <jlche18659@gmail.com>
2026-05-11 16:41:29 -0400

    Bro idfk what is going on with neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -245,6 +245,9 @@ if vim.g.vscode then
 		--
 		-- See `:help gitsigns` to understand what the configuration keys do
 		{
+			"VidocqH/auto-indent.nvim",
+		},
+		{
 			"numToStr/Comment.nvim",
 		},
 		{
@@ -389,13 +392,13 @@ else
 			end,
 		},
 
-		{
-			"vhyrro/luarocks.nvim",
-			priority = 1001, -- this plugin needs to run before anything else
-			opts = {
-				rocks = { "magick" },
-			},
-		},
+		-- {
+		-- 	"vhyrro/luarocks.nvim",
+		-- 	priority = 1001, -- this plugin needs to run before anything else
+		-- 	opts = {
+		-- 		rocks = { "magick" },
+		-- 	},
+		-- },
 		{
 			"ray-x/lsp_signature.nvim",
 			event = "VeryLazy",
@@ -455,107 +458,6 @@ else
 
 		{ -- Fuzzy Finder (files, lsp, etc)
 			"nvim-telescope/telescope.nvim",
-			event = "VimEnter",
-			branch = "0.1.x",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-				{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-					"nvim-telescope/telescope-fzf-native.nvim",
-
-					-- `build` is used to run some command when the plugin is installed/updated.
-					-- This is only run then, not every time Neovim starts up.
-					build = "make",
-
-					-- `cond` is a condition used to determine whether this plugin should be
-					-- installed and loaded.
-					cond = function()
-						return vim.fn.executable("make") == 1
-					end,
-				},
-				{ "nvim-telescope/telescope-ui-select.nvim" },
-
-				-- Useful for getting pretty icons, but requires a Nerd Font.
-				{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-			},
-			config = function()
-				-- Telescope is a fuzzy finder that comes with a lot of different things that
-				-- it can fuzzy find! It's more than just a "file finder", it can search
-				-- many different aspects of Neovim, your workspace, LSP, and more!
-				--
-				-- The easiest way to use Telescope, is to start by doing something like:
-				--  :Telescope help_tags
-				--
-				-- After running this command, a window will open up and you're able to
-				-- type in the prompt window. You'll see a list of `help_tags` options and
-				-- a corresponding preview of the help.
-				--
-				-- Two important keymaps to use while in Telescope are:
-				--  - Insert mode: <c-/>
-				--  - Normal mode: ?
-				--
-				-- This opens a window that shows you all of the keymaps for the current
-				-- Telescope picker. This is really useful to discover what Telescope can
-				-- do as well as how to actually do it!
-
-				-- [[ Configure Telescope ]]
-				-- See `:help telescope` and `:help telescope.setup()`
-				require("telescope").setup({
-					-- You can put your default mappings / updates / etc. in here
-					--  All the info you're looking for is in `:help telescope.setup()`
-					--
-					-- defaults = {
-					--   mappings = {
-					--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-					--   },
-					-- },
-					pickers = {},
-					extensions = {
-						["ui-select"] = {
-							require("telescope.themes").get_dropdown(),
-						},
-					},
-				})
-
-				-- Enable Telescope extensions if they are installed
-				pcall(require("telescope").load_extension, "fzf")
-				pcall(require("telescope").load_extension, "ui-select")
-
-				-- See `:help telescope.builtin`
-				local builtin = require("telescope.builtin")
-				vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-				vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-				vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-				vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-				vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-				vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-				vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-				vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-				vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-				vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-				-- Slightly advanced example of overriding default behavior and theme
-				vim.keymap.set("n", "<leader>/", function()
-					-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-					builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-						winblend = 10,
-						previewer = false,
-					}))
-				end, { desc = "[/] Fuzzily search in current buffer" })
-
-				-- It's also possible to pass additional configuration options.
-				--  See `:help telescope.builtin.live_grep()` for information about particular keys
-				vim.keymap.set("n", "<leader>s/", function()
-					builtin.live_grep({
-						grep_open_files = true,
-						prompt_title = "Live Grep in Open Files",
-					})
-				end, { desc = "[S]earch [/] in Open Files" })
-
-				-- Shortcut for searching your Neovim configuration files
-				vim.keymap.set("n", "<leader>sn", function()
-					builtin.find_files({ cwd = vim.fn.stdpath("config") })
-				end, { desc = "[S]earch [N]eovim files" })
-			end,
 		},
 
 		-- LSP Plugins
@@ -678,7 +580,7 @@ else
 						-- When you move your cursor, the highlights will be cleared (the second autocommand).
 						local client = vim.lsp.get_client_by_id(event.data.client_id)
 						if
-							client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
+							client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
 						then
 							local highlight_augroup =
 								vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
@@ -710,7 +612,7 @@ else
 						-- code, if the language server you are using supports them
 						--
 						-- This may be unwanted, since they displace some of your code
-						if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+						if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
 							map("<leader>th", function()
 								vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 							end, "[T]oggle Inlay [H]ints")
@@ -1055,7 +957,11 @@ else
 
 		{ -- Highlight, edit, and navigate code
 			"nvim-treesitter/nvim-treesitter",
+
+			tag = "v0.10.0",
+			lazy = false,
 			build = ":TSUpdate",
+
 			opts = {
 				ensure_installed = {
 					"bash",
@@ -1241,10 +1147,23 @@ else
 	vim.opt.expandtab = false
 	vim.opt.smartindent = false
 
-	require("nvim-treesitter.configs").setup({
-		indent = {
-			enable = true,
+	local configs = require("nvim-treesitter.configs")
+
+	configs.setup({
+		ensure_installed = {
+			"python",
+			"cpp",
+			"c",
+			"proto",
+			"dockerfile",
+			"starlark",
+			"bash",
+			"javascript",
+			"lua",
 		},
+		sync_install = false,
+		highlight = { enable = true },
+		indent = { enable = false },
 	})
 
 	-- require("lspconfig").clangd.setup({
